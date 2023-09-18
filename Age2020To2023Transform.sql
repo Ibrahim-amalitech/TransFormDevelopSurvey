b27427b9d5f56450a4528e9c7666397f7988c69e
Ibrahim-nnabaale <108925151+Ibrahim-nnabaale@users.noreply.github.com>
2023-09-18 14:43:32 +0000

    modifies some scripts

--- Age2020To2023Transform.sql
+++ Age2020To2023Transform.sql
@@ -1,21 +1,22 @@
 USE TrendStreamDB;
 
 -- Add a new column 'AgeCategory'
-ALTER TABLE [TrendStreamDB].[dbo].[dev_survey_2020_to_2023]
+ALTER TABLE [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey]
 ADD [AgeCategory] NVARCHAR(50); -- You can adjust the length (50) as needed
 
 -- Update 'AgeCategory' column based on 'Age'
-UPDATE [TrendStreamDB].[dbo].[dev_survey_2020_to_2023]
+UPDATE [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey]
 SET AgeCategory =
     CASE
         WHEN Age IS NULL OR Age = '' THEN 'Prefer not to say'
         WHEN TRY_CAST(Age AS FLOAT) IS NOT NULL THEN
             CASE
                 WHEN CAST(Age AS FLOAT) < 18 THEN 'Under 18 years old'
-                WHEN CAST(Age AS FLOAT) BETWEEN 18 AND 24 THEN '18-24 years old'
-                WHEN CAST(Age AS FLOAT) BETWEEN 25 AND 34 THEN '25-34 years old'
-                WHEN CAST(Age AS FLOAT) BETWEEN 35 AND 44 THEN '35-44 years old'
-                WHEN CAST(Age AS FLOAT) BETWEEN 45 AND 54 THEN '45-54 years old'
+                WHEN CAST(Age AS FLOAT) BETWEEN 18 AND 24.9 THEN '18-24 years old'
+                WHEN CAST(Age AS FLOAT) BETWEEN 25 AND 34.9 THEN '25-34 years old'
+                WHEN CAST(Age AS FLOAT) BETWEEN 35 AND 44.9 THEN '35-44 years old'
+                WHEN CAST(Age AS FLOAT) BETWEEN 45 AND 54.9 THEN '45-54 years old'
+				WHEN CAST(Age AS FLOAT) BETWEEN 55 AND 64.9 THEN '45-54 years old'
                 WHEN CAST(Age AS FLOAT) >= 65 THEN '65 years or older'
                 ELSE AgeCategory  -- Handle other cases as needed
             END
@@ -29,4 +30,6 @@ SET AgeCategory =
         ELSE AgeCategory  -- Handle other cases as needed
     END;
 
-	
+----validate changes
+--SELECT DISTINCT Age,AgeCategory
+--FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey];	
--- NormalizeCurrency.sql
+++ NormalizeCurrency.sql
@@ -1,17 +1,45 @@
 Use TrendStreamDB;
--- Declare a variable to store A_values
-DECLARE @A_values TABLE (
-    currency NVARCHAR(MAX)
-);
+---- Declare a variable to store A_values
+--DECLARE @A_values TABLE (
+--    currency NVARCHAR(MAX)
+--);
 
--- Insert A_values into the variable
-INSERT INTO @A_values (currency)
-SELECT currency
-FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded]
-WHERE currency COLLATE Latin1_General_BIN LIKE '[A-Z][^A-Z][^A-Z]%';
+---- Insert A_values into the variable
+--INSERT INTO @A_values (currency)
+--SELECT currency
+--FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded]
+--WHERE currency COLLATE Latin1_General_BIN LIKE '[A-Z][^A-Z][^A-Z]%';
 
--- Update the original table to replace A_values with corresponding B_values
+---- Update the original table to replace A_values with corresponding B_values
+--UPDATE a
+--SET a.CurrencyCheck = b.currency
+--FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded] a
+--INNER JOIN @A_values b ON a.currency LIKE '%' + b.currency + '%';
+
+-- --Add a new column 'AgeCategory'
+--ALTER TABLE [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey]
+--ADD [CurrencyCheck] NVARCHAR(50) NULL; -- You can adjust the length (50) as needed
+
 UPDATE a
-SET a.CurrencyCheck = b.currency
-FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded] a
-INNER JOIN @A_values b ON a.currency LIKE '%' + b.currency + '%';
+SET a.CurrencyCheck = 
+    CASE
+        -- When a_value satisfies the condition [A-Z][^A-Z][^A-Z]%
+        WHEN a.currency COLLATE Latin1_General_BIN LIKE '[A-Z][^A-Z][^A-Z]%'
+        THEN
+            CONCAT(
+                -- Get the first three characters of w.currency for concatenation
+                (SELECT TOP 1 
+                    LEFT(w.currency, 3)
+                 FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey] w
+                 WHERE CHARINDEX(a.currency, w.currency COLLATE Latin1_General_BIN) > 0
+                ),
+                ' ' + a.currency
+            )
+        ELSE a.Currency -- Use a.Currency when the condition is not met
+    END
+FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey] a;
+
+----validate changes
+--SELECT DISTINCT currency,CurrencyCheck
+--FROM [TrendStreamDB].[dbo].[dev_survey_2020_to_2023_utf8_encoded_primarykey]
+--;
